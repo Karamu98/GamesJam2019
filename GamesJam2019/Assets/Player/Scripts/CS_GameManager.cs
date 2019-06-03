35208f615107dfa8310a79f72ee00d1d37f35b10
KaryaS <kps224.ks@gmail.com>
2019-06-03 13:35:31 +0100

    Basic Player Controller

---
+++ GamesJam2019/Assets/Player/Scripts/CS_GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CS_GameManager : MonoBehaviour
+{
+
+    private bool[] bPlayersIn;
+    private int iNumOfPlayers;
+    [SerializeField]
+    private GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iNumOfPlayers = 0;
+        bPlayersIn = new bool[4];
+        for(int i = 0; i < 4; ++i)
+        {
+            bPlayersIn[i] = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ListenForPlayer();
+    }
+
+    private void ListenForPlayer()
+    {
+        if (Input.GetButtonDown("A1") && !bPlayersIn[0])
+        {
+            bPlayersIn[0] = true;
+            GameObject player = Instantiate(playerPrefab);
+            player.GetComponent<CS_PlayerController>().SetPlayerNumber(1);
+        }
+        if (Input.GetButtonDown("A2") && !bPlayersIn[1])
+        {
+            bPlayersIn[1] = true;
+            GameObject player = Instantiate(playerPrefab);
+            player.GetComponent<CS_PlayerController>().SetPlayerNumber(2);
+        }
+        if (Input.GetButtonDown("A3") && !bPlayersIn[2])
+        {
+            bPlayersIn[2] = true;
+            GameObject player = Instantiate(playerPrefab);
+            player.GetComponent<CS_PlayerController>().SetPlayerNumber(3);
+        }
+        if (Input.GetButtonDown("A4") && !bPlayersIn[3])
+        {
+            bPlayersIn[3] = true;
+            GameObject player = Instantiate(playerPrefab);
+            player.GetComponent<CS_PlayerController>().SetPlayerNumber(4);
+        }
+
+    }
+}
---
+++ GamesJam2019/Assets/Player/Scripts/CS_PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CS_PlayerController : MonoBehaviour
+{
+    private int iPlayerNum;
+    private NavMeshAgent nav;
+    private float fSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = gameObject.GetComponent<NavMeshAgent>();
+        fSpeed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+       nav.velocity = new Vector3(Input.GetAxis("Horizontal" + iPlayerNum) * fSpeed,
+         0, Input.GetAxis("Vertical" + iPlayerNum) * fSpeed);
+    }
+
+    public void SetPlayerNumber(int a_iNum)
+    {
+        iPlayerNum = a_iNum;
+    }
+}
