e011107c5d9d4276df6824af71513df987507a29
Daniel McCluskey <dmccluskey98+Uni@gmail.com>
2019-06-03 21:20:28 +0100

    AI - Added spawner

---
+++ GamesJam2019/Assets/Scripts/AISpawning/CS_AISpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_AISpawner : MonoBehaviour
+{
+
+    [Header("Settings")]
+    [SerializeField]
+    private int m_iMaxEnemies = 10;
+    private int m_iCurrentlySpawnedEnemies = 0;
+
+    [SerializeField]
+    private float m_fTimeBetweenSpawns = 5.0f;
+
+
+
+    [Header("Enemy Prefabs")]
+    [SerializeField]
+    private List<GameObject> m_lgoEnemyPrefabList = new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
