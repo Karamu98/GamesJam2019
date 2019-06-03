de43d7323811974c02138f4e5030e4e51d484d1c
KaryaS <kps224.ks@gmail.com>
2019-06-03 15:03:54 +0100

    Updated player with rotation

--- GamesJam2019/Assets/Player/Scripts/CS_PlayerController.cs
+++ GamesJam2019/Assets/Player/Scripts/CS_PlayerController.cs
@@ -3,32 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class CS_PlayerController : MonoBehaviour
+public class CS_PlayerController : MonoBehaviour, IDamageable
 {
     private int iPlayerNum;
-    private NavMeshAgent nav;
+    private Rigidbody nav;
     private float fSpeed;
+    private bool bStunned;
+    private float fTimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        nav = gameObject.GetComponent<NavMeshAgent>();
-        fSpeed = 5;
+        nav = gameObject.GetComponent<Rigidbody>();
+        fSpeed = 10f;
+        bStunned = false;
+        fTimer = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerMovement();
+        if(!bStunned)
+        {
+            PlayerMovement();
+        }
+        else
+        {
+            PassOut();
+        }
     }
 
     private void PlayerMovement()
     {
-       nav.velocity = new Vector3(Input.GetAxis("Horizontal" + iPlayerNum) * fSpeed,
+       //gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal" + iPlayerNum) * fSpeed,
+        // 0, Input.GetAxis("Vertical" + iPlayerNum) * fSpeed);
+        nav.velocity = new Vector3(Input.GetAxis("Horizontal" + iPlayerNum) * fSpeed,
          0, Input.GetAxis("Vertical" + iPlayerNum) * fSpeed);
+        CalculateRotation();
+    }
+
+    private void CalculateRotation()
+    {
+        float fx = Input.GetAxis("RotateHorizontal" + iPlayerNum);
+        Vector3 Look = new Vector3(fx, 0, -Input.GetAxisRaw("RotateVertical" + iPlayerNum));
+        Look += gameObject.transform.position;
+        Debug.Log(fx);
+        gameObject.transform.LookAt(Look);
+
     }
 
     public void SetPlayerNumber(int a_iNum)
     {
         iPlayerNum = a_iNum;
+    }
+
+    private void PassOut()
+    {
+        fTimer -= Time.deltaTime;
+        if(fTimer <= 0)
+        {
+            bStunned = false;
+            fTimer = 3;
+        }
+    }
+
+
+    public void TakeDamage(int a_damageToTake)
+    {
+        if (!bStunned)
+        {
+            bStunned = true;
+        }
+    }
+
+    public void Heal(int a_healthToHeal)
+    {
+        throw new System.NotImplementedException();
     }
 }
