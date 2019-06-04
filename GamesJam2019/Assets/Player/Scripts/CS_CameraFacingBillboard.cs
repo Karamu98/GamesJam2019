ffcf151cc1e166292e7831d807fdcce22a066754
KaryaS <kps224.ks@gmail.com>
2019-06-04 11:38:06 +0100

    Player UI p1

---
+++ GamesJam2019/Assets/Player/Scripts/CS_CameraFacingBillboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_CameraFacingBillboard : MonoBehaviour
+{
+    private GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(cam.transform);
+    }
+}
